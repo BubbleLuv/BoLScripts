d5eb4c65a642995ea26882e64ae582f1ebbf44bd
berbb <tintinridge@gmail.com>
2015-06-06 22:37:08 -0700

    Initial Release
    Release contains the sample template from NebelWolfi. The template is
    mostly edited to match Urgot's stats. More to come :)

---
+++ Urgot - Underated.lua
@@ -0,0 +1,422 @@
+--[[
+  _    _                 _              _    _           _                _           _ 
+ | |  | |               | |            | |  | |         | |              | |         | |
+ | |  | |_ __ __ _  ___ | |_   ______  | |  | |_ __   __| | ___ _ __ __ _| |_ ___  __| |
+ | |  | | '__/ _` |/ _ \| __| |______| | |  | | '_ \ / _` |/ _ \ '__/ _` | __/ _ \/ _` |
+ | |__| | | | (_| | (_) | |_           | |__| | | | | (_| |  __/ | | (_| | ||  __/ (_| |
+  \____/|_|  \__, |\___/ \__|           \____/|_| |_|\__,_|\___|_|  \__,_|\__\___|\__,_|
+              __/ |                                                                     
+             |___/                                                                      
+			 
+    By Berb
+]]--
+
+--[[ Auto updater start ]]--
+local version = 0.01
+local AUTO_UPDATE = true
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/berbb/BoL/master/Urgot - Underated.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Urgot - Underated.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+local function TopKekMsg(msg) print("<font color=\"#6699ff\"><b>[ADC Series]: Urgot - </b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTO_UPDATE then
+  local ServerData = GetWebResult(UPDATE_HOST, "/berbb/BoL/master/Urgot.version")
+  if ServerData then
+    ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+    if ServerVersion then
+      if tonumber(version) < ServerVersion then
+        TopKekMsg("New version available v"..ServerVersion)
+        TopKekMsg("Updating, please don't press F9")
+        DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () TopKekMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version") end) end, 3)
+      else
+        TopKekMsg("Loaded the latest version (v"..ServerVersion..")")
+      end
+    end
+  else
+    TopKekMsg("Error downloading version info")
+  end
+end
+--[[ Auto updater end ]]--
+
+--[[ Libraries start ]]--
+UPL = nil
+if FileExist(LIB_PATH .. "/UPL.lua") then
+  require("UPL")
+  UPL = UPL()
+else 
+  TopKekMsg("Downloading UPL, please don't press F9")
+  DelayAction(function() DownloadFile("https://raw.github.com/nebelwolfi/BoL/master/Common/UPL.lua".."?rand="..math.random(1,10000), LIB_PATH.."UPL.lua", function () TopKekMsg("Successfully downloaded UPL. Press F9 twice.") end) end, 3) 
+  return
+end
+
+if FileExist(LIB_PATH .. "SourceLib.lua") then
+  require("SourceLib")
+else
+  TopKekMsg("Please download SourceLib")
+  return
+end
+--[[ Libraries end ]]--
+
+--[[ Script start ]]--
+if myHero.charName ~= "Urgot" then return end -- not supported :(
+if VIP_USER then HookPackets() end
+if myHero:GetSpellData(SUMMONER_1).name:find("summonerdot") then Ignite = SUMMONER_1 elseif myHero:GetSpellData(SUMMONER_2).name:find("summonerdot") then Ignite = SUMMONER_2 end
+local QReady, WReady, EReady, RReady, IReady = function() return myHero:CanUseSpell(_Q) end, function() return myHero:CanUseSpell(_W) end, function() return myHero:CanUseSpell(_E) end, function() return myHero:CanUseSpell(_R) end, function() if Ignite ~= nil then return myHero:CanUseSpell(self.Ignite) end end
+local RebornLoaded, RevampedLoaded, MMALoaded, SxOrbLoaded, SOWLoaded = false, false, false, false, false
+local Target 
+local sts
+local predictions = {}
+local enemyTable = {}
+local enemyCount = 0
+local data = {
+  [_Q] = { speed = 1600, delay = 0.2, range = 1400, width = 80, collision = true, aoe = false, type = "linear"}, -- data[0]
+  [_E] = { speed = 1750, delay = 0.3, range = 920, width = 200, collision = false, aoe = true, type = "circular"} -- data[1]
+}
+
+function OnLoad()
+  Config = scriptConfig("Urgot - Underated", "Urgot - Underated")
+  
+  Config:addSubMenu("Pred/Skill Settings", "misc")
+  if VIP_USER then Config.misc:addParam("pc", "Use Packets To Cast Spells", SCRIPT_PARAM_ONOFF, false)
+  Config.misc:addParam("qqq", " ", SCRIPT_PARAM_INFO,"") end
+  Config.misc:addParam("qqq", "RELOAD AFTER CHANGING PREDICTIONS! (2x F9)", SCRIPT_PARAM_INFO,"")
+  Config.misc:addParam("pro",  "Type of prediction", SCRIPT_PARAM_LIST, 1, predToUse)
+
+  if ActivePred() == "DivinePred" then Config.misc:addParam("time","DPred Extra Time", SCRIPT_PARAM_SLICE, 0.03, 0, 0.3, 2) end
+  if ActivePred() == "HPrediction" then SetupHPred() end
+  
+  Config:addSubMenu("Misc settings", "casual")
+  --Nothing
+
+  Config:addSubMenu("Combo Settings", "comboConfig")
+  Config.comboConfig:addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+  Config.comboConfig:addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+  Config.comboConfig:addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+  Config.comboConfig:addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+  Config.comboConfig:addParam("items", "Use Items", SCRIPT_PARAM_ONOFF, true)
+
+  Config:addSubMenu("Ult Settings", "rConfig")
+  Config.rConfig:addParam("r", "Auto-R", SCRIPT_PARAM_ONOFF, true)
+  Config.rConfig:addParam("toomanyenemies", "Min. Allies for auto-r", SCRIPT_PARAM_SLICE, 3, 1, 5, 0)
+
+  Config:addSubMenu("Harrass Settings", "harrConfig")
+  Config.harrConfig:addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+  Config.harrConfig:addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+  Config.harrConfig:addParam("mana", "Min. mana %", SCRIPT_PARAM_SLICE, 30, 0, 100, 0)
+  
+  Config:addSubMenu("Lane Clear Settings", "farmConfig")
+  Config.farmConfig:addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+  Config.farmConfig:addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+  Config.farmConfig:addParam("mana", "Min. mana %", SCRIPT_PARAM_SLICE, 30, 0, 100, 0)
+      
+  Config:addSubMenu("Killsteal Settings", "KS")
+  Config.KS:addParam("enableKS", "Enable Killsteal", SCRIPT_PARAM_ONOFF, true)
+  Config.KS:addParam("killstealQ", "Use Q", SCRIPT_PARAM_ONOFF, true)
+  Config.KS:addParam("killstealE", "Use E", SCRIPT_PARAM_ONOFF, true)
+  if Ignite ~= nil then Config.KS:addParam("killstealI", "Use Ignite", SCRIPT_PARAM_ONOFF, true) end
+
+  Config:addSubMenu("Draw Settings", "Drawing")
+  Config.Drawing:addParam("QRange", "Q Range", SCRIPT_PARAM_ONOFF, true)
+  Config.Drawing:addParam("ERange", "E Range", SCRIPT_PARAM_ONOFF, true)
+  Config.Drawing:addParam("RRange", "R Range", SCRIPT_PARAM_ONOFF, true)
+  Config.Drawing:addParam("dmgCalc", "Damage", SCRIPT_PARAM_ONOFF, true)
+  
+  Config:addSubMenu("Key Settings", "kConfig")
+  Config.kConfig:addParam("combo", "SBTW (HOLD)", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+  Config.kConfig:addParam("harr", "Harrass (HOLD)", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
+  Config.kConfig:addParam("har", "Harrass (Toggle)", SCRIPT_PARAM_ONKEYTOGGLE, false, string.byte("G"))
+  Config.kConfig:addParam("lc", "Lane Clear (Hold)", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("V"))
+  
+  Config:addSubMenu("Orbwalk Settings", "oConfig")
+  SetupOrbwalk()
+
+  Config.kConfig:permaShow("combo")
+  Config.kConfig:permaShow("harr")
+  Config.kConfig:permaShow("har")
+  Config.kConfig:permaShow("lc")
+  
+  sts = SimpleTS(STS_PRIORITY_LESS_CAST_MAGIC)
+  Config:addSubMenu("Target Selector", "sts")
+  sts:AddToMenu(Config.sts)
+
+    for i = 1, heroManager.iCount do
+        local champ = heroManager:GetHero(i)
+        if champ.team ~= player.team then
+            enemyCount = enemyCount + 1
+            enemyTable[enemyCount] = { player = champ, name = champ.charName, damageQ = 0, damageE = 0, damageR = 0, indicatorText = "", damageGettingText = "", ready = true}
+        end
+    end
+end
+
+function SetupOrbwalk()
+  if _G.AutoCarry then
+    if _G.Reborn_Initialised then
+      RebornLoaded = true
+      TopKekMsg("Found SAC: Reborn")
+      Config.oConfig:addParam("Info", "SAC: Reborn detected!", SCRIPT_PARAM_INFO, "")
+    else
+      RevampedLoaded = true
+      TopKekMsg("Found SAC: Revamped")
+      Config.oConfig:addParam("Info", "SAC: Revamped detected!", SCRIPT_PARAM_INFO, "")
+    end
+  elseif _G.Reborn_Loaded then
+    DelayAction(function() SetupOrbwalk() end, 1)
+  elseif _G.MMA_Loaded then
+    MMALoaded = true
+    TopKekMsg("Found MMA")
+      Config.oConfig:addParam("Info", "MMA detected!", SCRIPT_PARAM_INFO, "")
+  elseif FileExist(LIB_PATH .. "SxOrbWalk.lua") then
+    require 'SxOrbWalk'
+    SxOrb = SxOrbWalk()
+    SxOrb:LoadToMenu(Config.oConfig)
+    SxOrbLoaded = true
+    TopKekMsg("Found SxOrb.")
+  elseif FileExist(LIB_PATH .. "SOW.lua") then
+    require 'SOW'
+    require 'VPrediction'
+    SOWVP = SOW(VP)
+    Config.oConfig:addParam("Info", "SOW settings", SCRIPT_PARAM_INFO, "")
+     Config.oConfig:addParam("Blank", "", SCRIPT_PARAM_INFO, "")
+    SOWVP:LoadToMenu(Config.oConfig)
+    SOWLoaded = true
+    TopKekMsg("Found SOW")
+  else
+    TopKekMsg("No valid Orbwalker found")
+  end
+end
+
+function OnTick()
+  local target
+  target = GetCustomTarget()
+  
+  if Config.rConfig.r then
+    local enemies = EnemiesAround(myHero, data[3].width)
+    if enemies >= Config.rConfig.toomanyenemies then
+      CastR(CastPosition, target)
+    end
+  end
+
+  if Config.KS.enableKS then 
+    Killsteal()
+  end
+
+  if Config.kConfig.har or Config.kConfig.harr then
+    Harrass()
+  end
+
+  if Config.kConfig.combo then
+    Combo()
+  end
+
+  if Config.kConfig.lc then
+    Farm()
+  end
+end
+
+function Farm()
+end
+
+function Combo()
+end
+
+function Harrass()
+end
+
+function EnemiesAround(Unit, range)
+  local c=0
+  for i=1,heroManager.iCount do hero = heroManager:GetHero(i) if hero.team ~= myHero.team and hero.x and hero.y and hero.z and GetDistance(hero, Unit) < range then c=c+1 end end return c
+end
+
+function Killsteal()
+  for i=1, heroManager.iCount do
+    local enemy = heroManager:GetHero(i)
+    local qDmg = ((getDmg("Q", enemy, myHero)) or 0)  
+    local eDmg = ((getDmg("E", enemy, myHero)) or 0)  
+    local rDmg = ((getDmg("R", enemy, myHero)) or 0)
+    local iDmg = 50 + (20 * myHero.level) / 5
+    if ValidTarget(enemy) and enemy ~= nil and not enemy.dead and enemy.visible then
+      if enemy.health < qDmg and Config.KS.killstealQ and ValidTarget(enemy, data[0].range) then
+        CastQ(enemy)
+      elseif enemy.health < eDmg and Config.KS.killstealE and ValidTarget(enemy, data[2].range) then
+        CastE(enemy)
+      elseif enemy.health < rDmg and Config.KS.killstealR and ValidTarget(enemy, data[3].range) then
+        CastR(enemy)
+      elseif enemy.health < iDmg and Config.KS.killstealI and ValidTarget(enemy, 600) and IReady then
+        CCastSpell(Ignite, enemy)
+      end
+    end
+  end
+end
+
+function CastQ(unit)
+end
+
+function CastW(unit)
+end
+
+function CastE(unit)
+end
+
+function CastR(unit)
+end
+
+local CastableItems = {
+  Tiamat      = { Range = 400, Slot = function() return GetInventorySlotItem(3077) end,  reqTarget = false, IsReady = function() return (GetInventorySlotItem(3077) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3077)) == READY) end, Damage = function(target) return getDmg("TIAMAT", target, myHero) end},
+  Hydra       = { Range = 400, Slot = function() return GetInventorySlotItem(3074) end,  reqTarget = false, IsReady = function() return (GetInventorySlotItem(3074) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3074)) == READY) end, Damage = function(target) return getDmg("HYDRA", target, myHero) end},
+  Bork        = { Range = 450, Slot = function() return GetInventorySlotItem(3153) end,  reqTarget = true, IsReady = function() return (GetInventorySlotItem(3153) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3153)) == READY) end, Damage = function(target) return getDmg("RUINEDKING", target, myHero) end},
+  Bwc         = { Range = 400, Slot = function() return GetInventorySlotItem(3144) end,  reqTarget = true, IsReady = function() return (GetInventorySlotItem(3144) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3144)) == READY) end, Damage = function(target) return getDmg("BWC", target, myHero) end},
+  Hextech     = { Range = 400, Slot = function() return GetInventorySlotItem(3146) end,  reqTarget = true, IsReady = function() return (GetInventorySlotItem(3146) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3146)) == READY) end, Damage = function(target) return getDmg("HXG", target, myHero) end},
+  Blackfire   = { Range = 750, Slot = function() return GetInventorySlotItem(3188) end,  reqTarget = true, IsReady = function() return (GetInventorySlotItem(3188) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3188)) == READY) end, Damage = function(target) return getDmg("BLACKFIRE", target, myHero) end},
+  Youmuu      = { Range = 350, Slot = function() return GetInventorySlotItem(3142) end,  reqTarget = false, IsReady = function() return (GetInventorySlotItem(3142) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3142)) == READY) end, Damage = function(target) return 0 end},
+  Randuin     = { Range = 500, Slot = function() return GetInventorySlotItem(3143) end,  reqTarget = false, IsReady = function() return (GetInventorySlotItem(3143) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3143)) == READY) end, Damage = function(target) return 0 end},
+  TwinShadows = { Range = 1000, Slot = function() return GetInventorySlotItem(3023) end, reqTarget = false, IsReady = function() return (GetInventorySlotItem(3023) ~= nil and myHero:CanUseSpell(GetInventorySlotItem(3023)) == READY) end, Damage = function(target) return 0 end},
+}
+
+function UseItems(unit)
+    if unit ~= nil then
+        for _, item in pairs(CastableItems) do
+            if item.IsReady() and GetDistance(myHero, unit) < item.Range then
+                if item.reqTarget then
+                    CastSpell(item.Slot(), unit)
+                else
+                    CastSpell(item.Slot())
+                end
+            end
+        end
+    end
+end
+
+function CCastSpell(Spell, xPos, zPos)
+  if not xPos and not zPos then
+    if VIP_USER and Config.misc.pc then
+        Packet("S_CAST", {spellId = Spell}):send()
+    else
+        CastSpell(Spell)
+    end
+  elseif xPos and not zPos then
+    target = xPos
+    if VIP_USER and Config.misc.pc then
+        Packet("S_CAST", {spellId = Spell, targetNetworkId = target.networkID}):send()
+    else
+        CastSpell(Spell, target)
+    end
+  elseif xPos and zPos then
+    if VIP_USER and Config.misc.pc then
+      Packet("S_CAST", {spellId = Spell, fromX = xPos, fromY = zPos, toX = xPos, toY = zPos}):send()
+    else
+      CastSpell(Spell, xPos, zPos)
+    end
+  end
+end
+
+function GetCustomTarget()
+    if _G.MMA_Target and _G.MMA_Target.type == myHero.type then return _G.MMA_Target end
+    if _G.AutoCarry and _G.AutoCarry.Crosshair and _G.AutoCarry.Attack_Crosshair and _G.AutoCarry.Attack_Crosshair.target and _G.AutoCarry.Attack_Crosshair.target.type == myHero.type then return _G.AutoCarry.Attack_Crosshair.target end
+    return sts:GetTarget(data[1].range)
+end
+
+function OnDraw()
+  if Config.Drawing.QRange then
+    DrawCircle(myHero.x, myHero.y, myHero.z, data[0].range+data[0].width/4, 0x111111)
+  end
+  if Config.Drawing.ERange then
+    DrawCircle(myHero.x, myHero.y, myHero.z, data[2].range+data[2].width/4, 0x111111)
+  end
+  if Config.Drawing.RRange then
+    DrawCircle(myHero.x, myHero.y, myHero.z, data[3].range+data[3].width/4, 0x111111)
+  end
+  if Config.Drawing.DmgCalcs then
+        for i = 1, enemyCount do
+            local enemy = enemyTable[i].player
+            if ValidTarget(enemy) then
+                local barPos = WorldToScreen(D3DXVECTOR3(enemy.x, enemy.y, enemy.z))
+                local posX = barPos.x - 35
+                local posY = barPos.y - 50
+                -- Doing damage
+                DrawText(enemyTable[i].indicatorText, 15, posX, posY, (enemyTable[i].ready and colorIndicatorReady or colorIndicatorNotReady))
+               
+                -- Taking damage
+                DrawText(enemyTable[i].damageGettingText, 15, posX, posY + 15, ARGB(255, 255, 0, 0))
+            end
+        end
+    end      
+end
+
+local colorRangeReady        = ARGB(255, 200, 0,   200)
+local colorRangeComboReady   = ARGB(255, 255, 128, 0)
+local colorRangeNotReady     = ARGB(255, 50,  50,  50)
+local colorIndicatorReady    = ARGB(255, 0,   255, 0)
+local colorIndicatorNotReady = ARGB(255, 255, 220, 0)
+local colorInfo              = ARGB(255, 255, 50,  0)
+local KillText = {}
+local KillTextColor = ARGB(255, 216, 247, 8)
+local KillTextList = {"Harass Him", "Combo Kill"}
+function DmgCalculations()
+    if not Config.Drawing.DmgCalcs then return end
+    for i = 1, enemyCount do
+        local enemy = enemyTable[i].player
+          if ValidTarget(enemy) and enemy.visible then
+            local damageAA = GetDmg("AD", enemy, player)
+            local damageQ  = GetDmg("Q", enemy, player)
+            local damageE  = GetDmg("E", enemy, player)
+            local damageI  = Ignite and (GetDmg("IGNITE", enemy)) or 0
+            enemyTable[i].damageQ = damageQ
+            enemyTable[i].damageE = damageE
+            if enemy.health < damageQ then
+                enemyTable[i].indicatorText = "Q Kill"
+                enemyTable[i].ready = QReady()
+            elseif enemy.health < damageE then
+                enemyTable[i].indicatorText = "E Kill"
+                enemyTable[i].ready = EReady()
+            elseif enemy.health < damageE + damageQ then
+                enemyTable[i].indicatorText = "Q + E Kill"
+                enemyTable[i].ready = EReady() and QReady()
+            else
+                local damageTotal = damageQ + damageE
+                local healthLeft = math.round(enemy.health - damageTotal)
+                local percentLeft = math.round(healthLeft / enemy.maxHealth * 100)
+                enemyTable[i].indicatorText = percentLeft .. "% Harass"
+                enemyTable[i].ready = QReady() or WReady() or EReady() or RReady()
+            end
+
+            local enemyDamageAA = getDmg("AD", player, enemy)
+            local enemyNeededAA = math.ceil(player.health / enemyDamageAA)            
+            enemyTable[i].damageGettingText = enemy.charName .. " kills me with " .. enemyNeededAA .. " hits"
+        end
+    end
+end
+
+function GetDmg(spell, enemy)
+  if enemy == nil then
+    return
+  end
+  
+  local ADDmg = 0
+  local APDmg = 0
+
+  local Level = myHero.level
+  local TotalDmg = myHero.totalDamage
+  local AP = myHero.ap
+  local ArmorPen = myHero.armorPen
+  local ArmorPenPercent = myHero.armorPenPercent
+  local MagicPen = myHero.magicPen
+  local MagicPenPercent = myHero.magicPenPercent
+  
+  local Armor = math.max(0, enemy.armor*ArmorPenPercent-ArmorPen)
+  local ArmorPercent = Armor/(100+Armor)
+  local MagicArmor = math.max(0, enemy.magicArmor*MagicPenPercent-MagicPen)
+  local MagicArmorPercent = MagicArmor/(100+MagicArmor)
+
+  local QLevel, WLevel, ELevel, RLevel = myHero:GetSpellData(_Q).level, myHero:GetSpellData(_W).level, myHero:GetSpellData(_E).level, myHero:GetSpellData(_R).level
+
+  if spell == "IGNITE" then
+    return 50+20*Level
+  elseif spell == "AD" then
+    ADDmg = TotalDmg
+  elseif spell == "Q" then
+    ADDmg = (30 * QLevel + -20) + (TotalDmg * .85)
+  elseif spell == "E" then
+    ADDmg = (55 * ELevel + 20) + (TotalDmg * .6)
+  end
+  return ADDmg*(1-ArmorPercent)+APDmg*(1-MagicArmorPercent)
+end
